aa987a594a64228b081297191757233f68b6dcd1
Bautista <bautistamoyano1606@gmail.com>
2025-04-07 09:50:20 -0300

    agregamos una ruta usando argumentos

--- app.py
+++ app.py
@@ -8,4 +8,10 @@ def boca():
 
 @app.route("/river")
 def river():
-    return "<p>Aguante River</p>"    
+    return "<p>Aguante River</p>"   
+
+@app.route("/tirar-dado<int:caras")
+def caras():
+    from random import randint
+    n = randint(1,caras)
+    return f"<p>Tire un dado de {caras} caras, slaio {n}</p>"   

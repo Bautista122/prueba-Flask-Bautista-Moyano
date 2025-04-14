7e6ee1f740b0918ac0ee72b7abda33ce8700427d
Bautista <bautistamoyano1606@gmail.com>
2025-04-14 12:02:28 -0300

    Agregamos sql a nuestra pagina para ver a otros usuarios con y sin argumentos

--- app.py
+++ app.py
@@ -1,18 +1,84 @@
-from flask import flask, url_for
 
-app = flask(__name__)
+from flask import Flask, url_for
+import sqlite3
 
-@app.route("/boca")
-def boca():
-    return "<p>Aguante Boca</p>"
 
-@app.route("/river")
-def river():
-    return "<p>Aguante River</p>"   
+app = Flask(__name__)
 
-@app.route("/tirar-dado<int:caras")
-def caras():
-    from random import randint
-    n = randint(1,caras)
-    return f"<p>Tire un dado de {caras} caras, slaio {n}</p>"   
 
+def dict_factory(cursor, row):
+   """Arma un diccionario con los valores de la fila."""
+   fields = [column[0] for column in cursor.description]
+   return {key: value for key, value in zip(fields, row)}
+
+
+def AbrirConexion():
+   global db
+   db = sqlite3.connect("instance/datos.sqlite")
+   db.row_factory = dict_factory
+
+
+def CerrarConexion():
+   global db
+   db.close
+   db = None
+
+
+@app.route("/test-db")
+def testDB():
+   AbrirConexion()
+   cursor = db.cursor()
+   cursor.execute("SELECT COUNT(*) AS cant FROM usuarios: ")
+   res = cursor.fetchone
+   registro = res["cant"]
+   CerrarConexion()
+   return f"Hay {registro} registros en la tabla usuarios"
+
+##sin argumentos
+@app.route("/crear-usuario")
+def testCrear():
+   nombre = "leandro"
+   email = "leandro@etec.uba.ar"
+   AbrirConexion()
+   cursor = db.cursor()
+   consulta = "INSERT INTO usuarios(usuario, email) VALUES (?,?)"
+   cursor.execute(consulta, (nombre, email))
+   db.commit()
+   CerrarConexion()
+   return f"Registro agregado ({nombre})"
+
+##con ARGUMENTOS
+@app.route("/crear-usuario/<string:nombre>/<string:email>")
+def testCrear(nombre,email):
+   AbrirConexion()
+   cursor = db.cursor()
+   consulta = "INSERT INTO usuarios(usuario, email) VALUES (?,?)"
+   cursor.execute(consulta, (nombre, email))
+   db.commit()
+   CerrarConexion()
+   return f"Registro agregado ({nombre})"
+
+
+@app.route("/")
+def main():
+   url_hola = url_for("hello")
+   url_dado = url_for("dados", Caras=6)
+   url_logo = url_for("static", filename="img/logo.jpg")
+   return f"""
+   <a href="{url_hola}">Hola</a>
+   <br>
+   <a href="{url_for("bye")}">Chau</a>
+   <br>
+   <a href="{url_logo}">Logo</a>
+   <br>
+   <a href="{url_dado}">Tirar_Dados</a>
+   """
+@app.route("/hola")
+def hello():
+   return """
+   <p>Hola</p>
+   <br>
+   <a href="/Chau">Chau</a>
+   <br>
+   <a href="Chau">Chau2</a>
+"""
